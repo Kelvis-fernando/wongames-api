6b5ed3cbb09f0323287ed4cf0ae9d996a1898b3e
Kelvis Delapena <kelvisdelapena@gmail.com>
2025-01-16 18:17:03 -0300

    fix: types

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -451,15 +451,6 @@ export interface ApiGameGame extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    description: Schema.Attribute.RichText &
-      Schema.Attribute.CustomField<
-        'plugin::ckeditor.CKEditor',
-        {
-          licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3Njg2MDc5OTksImp0aSI6IjNjZjM1NTAwLTU1OTQtNDI4OS1iZjhjLWRiYTNjZDc5OTUyMSIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiNjg3NzQwN2QifQ.stUaqA3-PnL3d64vw-eL7z3NK1-ziYbxZOOxj6vgJpzSY1x77QJgG6_CDsS9qrO4XRZ7I3cGZEJVke8QmdTJ4Q';
-          output: 'HTML';
-          preset: 'rich';
-        }
-      >;
     developers: Schema.Attribute.Relation<
       'manyToMany',
       'api::developer.developer'
