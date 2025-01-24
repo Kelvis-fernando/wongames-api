d2d35ace8951331e5e9768f69303ce5c7dd51270
Kelvis Delapena <kelvisdelapena@gmail.com>
2025-01-24 11:19:37 -0300

    feat: add querystring and add data to strapi

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -1,14 +1,22 @@
+/**
+ * game controller
+ */
+
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
   "api::game.game",
   ({ strapi }) => ({
     async populate(ctx) {
-      console.log("Starting to populate...");
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
 
-      await strapi.service("api::game.game").populate(ctx.query);
+      await strapi.service("api::game.game").populate(options);
 
-      ctx.send("Finishing populating...");
+      ctx.send("Finished populating games!");
     },
   })
 );
